5c07d1395a6c8cbbe49a506bf2b65246247e8cc8
Dominik Haas <dominik.haas@qaware.de>
2019-05-06 20:35:42 +0200

    Musterloesung Uebung 5

---
+++ codebase/u05-mvp-components/src/de/fhrosenheim/gui/u05/header/HeaderActionListener.java
@@ -0,0 +1,10 @@
+package de.fhrosenheim.gui.u05.header;
+
+/**
+ * Action Listener Interface
+ * 
+ * @author dominik.haas
+ */
+public interface HeaderActionListener {
+    void onGenerateGraph();
+}
